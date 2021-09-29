7332dc66b8cdb8ae7071f1b75d23168e809b11b6
carolyn99240 <59831525+carolyn99240@users.noreply.github.com>
2021-09-29 18:47:59 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -5,3 +5,4 @@ sudo apt install rclone wget rtorrent -y;
 cd /workspace/
 wget https://get.rapidlinks.xyz/0:/rclone.conf;
 sudo cp rclone.conf /root/.config/rclone/ ;
+sudo cp /home/gitpod/.config/rclone/rclone.conf;
